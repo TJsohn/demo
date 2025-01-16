7805e999eacb0bdfc07e27625312a56c2d11627f
TJsohn <sohntaejeong@gmail.com>
2025-01-16 14:54:52 +0200

    end of lesson commit

---
+++ week1/data-types/data_types.js
@@ -0,0 +1,33 @@
+//1. Create a variable called age and assign it a number (e.g., 25).
+let age = 25;
+
+//2. Create a variable called name and assign it a string (e.g., "John").
+const name = "John";
+
+//3. Create a variable called isStudent and assign it a boolean value (true or false).
+const isStudent = true;
+
+//4. Create a variable called hobbies and assign it an array with a few strings (e.g., ["reading", "coding", "running"]).
+const hobbies = ["reading", "coding", "running"];
+
+//5. Create an object called info with two properties:
+//schoolName (a string, e.g., "Business College Helsinki")
+//groupName (a number, e.g., REACT25K)
+const info = {
+  schoolName: "Business College Helsinki",
+  groupName: 25,
+};
+
+//6. Use console.log() to print each variable to the console.
+console.log(age);
+console.log(name);
+console.log(isStudent);
+console.log(hobbies);
+console.log(info);
+
+//7. Use the typeof operator to check and print the data type of each variable.
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof isStudent);
+console.log(typeof hobbies);
+console.log(typeof info);
---
+++ week1/operators/operators_01.js
@@ -0,0 +1,30 @@
+/* 
+1. Create two variables, a and b, and assign them integer values (e.g., 8 and 3).
+2. Perform the following operations using a and b, and store the results in separate variables:
+ - Add the values (sum).
+ - Subtract b from a (difference).
+ - Multiply the values (product).
+ - Divide a by b (quotient).
+ - Find the remainder when a is divided by b (remainder).
+3. Use console.log() to print the results of each operation. For example, print: "The sum of a and b is: 11"
+4. Add comments to explain what each operation does.
+5. Change the values of a and b and observe how the results change.
+6. Try the same operations with one positive and one negative number.
+*/
+let a = 5; // Creating a variable a and assigning it an integer value 5
+let b = 3; // Creating a varialbe b and assigning it an integer value 3
+
+let sum = a + b; //
+console.log("Sum of a and b is:", sum);
+
+let subtract = a - b;
+console.log("Subtract of a and b is:", subtract);
+
+let multiply = a * b;
+console.log("Multiply of a and b is:", multiply);
+
+let divide = a / b;
+console.log("Divide a by b is:", divide);
+
+let remainder = a % b;
+console.log("The remainder when a is divided by b is:", remainder);
